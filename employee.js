a8ccc4b1a103e92d56a8ff7f63243c9b24f1207e
kpranath <kpranath@gmail.com>
2020-02-09 14:43:04 +0530

    first commit

---
+++ employee.js
@@ -0,0 +1,22 @@
+var emp = [
+    {
+        id: 1,
+        name: "Shivu",
+        Role: "Developer"
+    },
+    {
+        id: 2,
+        name: "Harsh",
+        Role: "Developer"
+    },
+    {
+        id: 3,
+        name: "Gowtham",
+        Role: "Analyst"
+    },
+    {
+        id: 4,
+        name: "Chandana",
+        Role: "Tester"
+    }
+]
